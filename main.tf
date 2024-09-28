f3dcc574024bccebc4544b02a5aac951d16c7c59
poornatejav <poornateja340@gmail.com>
2024-09-28 18:17:41 -0500

    local change

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ data "aws_ecrpublic_authorization_token" "token" {
 
 locals {
   name   = "karpentercluster"
-  region = "eu-east-1"
+  region = "us-east-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
